8a2b2348b2410f49f0f8ea3539a51c56a1fc63a6
Gavin Lee <lee.gavin.k@gmail.com>
2023-01-03 19:46:55 +0100

    code more

--- main.py
+++ main.py
@@ -48,13 +48,3 @@ def read_root():
 @app.get("/path")
 async def demo_get():
     return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
